e3ec0596c556e2793e3d7d39ce22838303e1d3a0
Greg Linklater <greglink49@gmail.com>
2016-09-25 14:51:10 +0200

    doc(JACL): added JSDOCs to the source

--- src/JACL.js
+++ src/JACL.js
@@ -1,10 +1,33 @@
 'use strict'
 
+/**
+ * Module dependencies
+ * @ignore
+ */
 const Rule = require('./Rule')
 
+/**
+ * JACL
+ *
+ * @class
+ * JACL is a high level interface for the 'JACL Authorization Engine'. Instances
+ * of this class allow multiple rules to be registered and cached before
+ * evaluation is required.
+ */
 class JACL {
 
-  constructor (rules) {
+  /**
+   * Constructor
+   *
+   * @description
+   * Creates a new instance of JACL Auth Engine. Optionally takes a dictionary
+   * of rules and registers them. This is to be used for the case of
+   * deserializing existing rules from a store.
+   * 
+   * @param  {Object} [rules=Empty Object] - 
+   * Rules dictionary (k,v) => (rule name, rule schema)
+   */
+  constructor (rules={}) {
     this.rules = {}
     this.attributes = {}
     Object.keys(rules).forEach(key => {
@@ -12,6 +35,15 @@ class JACL {
     })
   }
 
+  /**
+   * Register
+   *
+   * @description
+   * Register a new named rule on the authorization engine.
+   * 
+   * @param  {string} name - Rule name
+   * @param  {Object} rule - Rule schema
+   */
   register (name, rule) {
     try {
       let r = new Rule(rule)
@@ -23,6 +55,19 @@ class JACL {
     }
   }
 
+  /**
+   * Enforce
+   *
+   * @description
+   * Determine if an access request should be granted or denied from the
+   * attributes of the subject, resource and environment.
+   * 
+   * @param  {string} name - Rule name
+   * @param  {Object} sub - Subject attributes
+   * @param  {Object} obj - Resource attributes
+   * @param  {Object} env - Environment attributes
+   * @return {boolean} Rule enforcement decision (true/false) => (allow/deny)
+   */
   enforce (name, sub, obj, env) {
     if (this.rules[name]) {
       let rule = this.rules[name]
@@ -31,6 +76,17 @@ class JACL {
     return false // Fail quietly
   }
 
+  /**
+   * Attributes List
+   *
+   * @description
+   * Get the list of attributes required for the evaluation of a named rule.
+   * 
+   * @param  {string} name - Rule name
+   * @param  {string} [type='all'] - Attribute type
+   * @return {Array} An array of JSON Pointer strings of required attributes
+   * needed for evaluation of the named rule.
+   */
   attributesList (name, type) {
     if (this.attributes[name]) {
       if (type === 'subject') {
@@ -48,4 +104,7 @@ class JACL {
 
 }
 
+/**
+ * Exports
+ */
 module.exports = JACL
